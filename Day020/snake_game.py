f7c7f61ded6fa0adb2b612886a2ea81b51478282
Lousada <ricardo.lousada@fujitsu.com>
2023-08-07 10:58:39 +0100

    Day21 Commit

--- Day020/snake_game.py
+++ Day020/snake_game.py
@@ -3,6 +3,8 @@
 from turtle import Screen
 from snake import Snake
 import time
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,7 +13,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-#food = Food()
+food = Food()
+score_board = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,8 +26,22 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score_board.increase_score()
+    # detect collision with the wall
+    if snake.head.xcor() > 300 or snake.head.xcor() < - 300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        game_is_on = False
+        score_board.game_over()
+    # detect collision with the tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
+
 
 
 screen.exitonclick()
