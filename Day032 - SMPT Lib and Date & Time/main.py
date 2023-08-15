4fe9bae0b90a2bc6cea0287acc78543fe44ac1ac
Lousada <ricardo.lousada@fujitsu.com>
2023-08-15 16:48:46 +0100

    deleted some data files

--- Day032 - SMPT Lib and Date & Time/main.py
+++ Day032 - SMPT Lib and Date & Time/main.py
@@ -3,8 +3,8 @@ import smtplib, ssl
 
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 587
-SENDER_EMAIL= "lousadaricardo5@gmail.com"
-SENDER_PASSWORD ="vkqabtgyjoxdyhzo"
+SENDER_EMAIL= "XXXXX
+SENDER_PASSWORD ="XXXXX"
 
 
 #create a secure SSL context
@@ -15,7 +15,7 @@ try:
     server = smtplib.SMTP(SMTP_SERVER,PORT)
     server.starttls(context=context)
     server.login((SENDER_EMAIL,SENDER_PASSWORD))
-    server.sendmail(SENDER_EMAIL,"ricardo.lousada@gmail.com",msg="Hello")
+    server.sendmail(SENDER_EMAIL,"",msg="Hello")
 except Exception as error:
     print(error)
 finally:
