9d89a63c1655d1fdf71aa3cf1f70aa83614fc601
ricardolousada <ricardo.lousada@gmail.com>
2023-09-20 08:47:25 +0100

    Added env files

--- Day071 - Deploying your Web Application/main.py
+++ Day071 - Deploying your Web Application/main.py
@@ -9,6 +9,8 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
+from dotenv import load_dotenv
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -26,9 +28,9 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLAK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -53,7 +55,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 db = SQLAlchemy()
 db.init_app(app)
 
